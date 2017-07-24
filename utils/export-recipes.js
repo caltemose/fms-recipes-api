8063228eccb061413f73404190af6c835110180d
Chad Altemose <caltemose@hugeinc.com>
2017-07-24 11:28:51 -0400

    initial export-recipes script functional

---
+++ utils/export-recipes.js
@@ -0,0 +1,56 @@
+const path = require('path')
+const fs = require('fs')
+const assert = require('assert')
+const mongoose = require('mongoose')
+mongoose.Promise = require('bluebird')
+
+require('../models/')
+
+const config = require('../config/config.js')
+const recipesDb = require('../helpers/db/recipes')
+
+const done = (cb) => {
+    mongoose.disconnect( () => {
+        console.log('')
+        console.log('disconnected from mongo')
+        if (cb) cb()
+    })
+}
+
+const writeRecipeToFile = (recipe, last) => {
+    let fileout = `../exports/${recipe.slug}.json`
+    fileout = path.resolve(__dirname, fileout)
+    fs.writeFile(fileout, JSON.stringify(recipe, null, 2), (err) => {
+        if (err) {
+            done(console.log(err))
+            throw err
+        }
+        console.log(recipe.slug, 'written to:', fileout)
+        if (last) done(console.log('done writing files.'))
+    })
+}
+
+const exportRecipes = recipes => {
+    recipes.forEach((recipe, index) => {
+        const last = index === recipes.length -1
+        recipesDb.getRecipeById(recipe._id)
+            .then(recipe => {
+                writeRecipeToFile(recipe, last)
+            })
+            .catch(err => {
+                done(console.error(err))
+            })
+    })
+}
+
+mongoose.connect(config.dbHost + config.devDb, (err) => {
+    assert.equal(null, err)
+    console.log('connected to', mongoose.connection.db.databaseName)
+    recipesDb.getRecipes()
+        .then(docs => {
+            exportRecipes(docs)
+        })
+        .catch(err => {
+            done(console.error(err))
+        })
+})
